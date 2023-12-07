26e6215b7b1c5212cedfd3bf3315befc8b61a981
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2023-12-07 20:11:43 +0200

    initial commit

---
+++ Laboratorna_6_2_ITER.Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Laboratorna_6_2_ITER.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestFindMinMax()
+        {
+            const int n = 5;
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            int minValue, maxValue;
+            Program.FindMinMax(arr, n, out minValue, out maxValue);
+
+            Assert.AreEqual(1, minValue);
+            Assert.AreEqual(5, maxValue);
+        }
+    }
+}
---
+++ Laboratorna_6_2_ITER/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Laboratorna_6_2_ITER
+{
+    public class Program
+    {
+        static void FillArray(int[] arr, int n)
+        {
+            Console.WriteLine($"Enter {n} elements for the array:");
+            for (int i = 0; i < n; ++i)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void PrintArray(int[] arr, int n)
+        {
+            Console.Write("Array: ");
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void FindMinMax(int[] arr, int n, out int min, out int max)
+        {
+            min = int.MaxValue;
+            max = int.MinValue;
+
+            for (int i = 0; i < n; ++i)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter the size of the array: ");
+            if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+            {
+                Console.Error.WriteLine("Invalid array size");
+                return;
+            }
+
+            int[] arr = new int[n];
+
+            FillArray(arr, n);
+            PrintArray(arr, n);
+
+            int minValue, maxValue;
+            FindMinMax(arr, n, out minValue, out maxValue);
+
+            Console.WriteLine($"Min: {minValue}");
+            Console.WriteLine($"Max: {maxValue}");
+        }
+    }
+}
